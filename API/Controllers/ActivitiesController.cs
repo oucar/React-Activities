800f81d811cd200320d1b7cb30411f6720ea785b
oucar <hello@ucaronur.com>
2023-07-15 18:45:11 -0400

    Initial Reactivities Base Controller and Activities Controller Implementation

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,32 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : ReactivitiesBaseController
+    {
+        private readonly DataContext _dataContext;
+        public ActivitiesController(DataContext context)
+        {
+            _dataContext = context;
+        }
+
+
+        // api/activities
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _dataContext.Activities.ToListAsync();
+        }
+
+        // api/activities/${id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetSingleActivity(Guid id)
+        {
+            return await _dataContext.Activities.FindAsync(id);
+        }
+
+    }
+}
