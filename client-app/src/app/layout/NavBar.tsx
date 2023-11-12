9966a3f4d7dae7fabe169f05db1812943922e0cb
oucar <onurucar@iu.edu>
2023-11-12 04:50:22 -0500

    Added images, added more styling around the navbar, added activity list and activity dashboard components

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Button, Container, Menu } from "semantic-ui-react";
+import "./styles.css";
 
 export default function Navbar() {
   return (
     <Menu inverted fixed="top">
       <Container>
         <Menu.Item header>
-          <img src="/assets/logo.png" alt="logo" />
+          <img src="/public/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/>
           Reactivities
         </Menu.Item>
         <Menu.Item name='Activities'/>
