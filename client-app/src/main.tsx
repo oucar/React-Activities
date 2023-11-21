6ada9894ca6e3d4da99d96c7a2f5b020b118c221
oucar <hello@ucaronur.com>
2023-11-20 23:27:16 -0500

    Initial setup of MobX

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,5 +1,14 @@
 import ReactDOM from "react-dom/client";
 import App from "./app/layout/App.tsx";
 import "semantic-ui-css/semantic.min.css";
+import React from "react";
+import { StoreContext, store } from "./app/stores/store.ts";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    {/* Setting up MobX */}
+    <StoreContext.Provider value={store}>
+      <App />
+    </StoreContext.Provider>
+  </React.StrictMode>
+);
