d98f4534e8587dcdd8036422702d832e7d4a249d
oucar <onurucar@iu.edu>
2023-11-26 17:41:01 -0500

    Refactored Delete logic, done implementing a global store using MobX

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,9 +1,7 @@
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useEffect } from "react";
 import { Container } from "semantic-ui-react";
-import { Activity } from "../models/activity";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import agent from "../api/agent";
 import LoadingComponent from "./LoadingComponent";
 import { useStore } from "../stores/store";
 import { observer } from "mobx-react-lite";
@@ -12,25 +10,12 @@ function App() {
   // destructure the activityStore from the MobX store
   const { activityStore } = useStore();
 
-  const [activities, setActivities] = useState<Activity[]>([]);
-  const [isSubmitting, setIsSubmitting] = useState(false);
-
-
   // load activities when the component mounts
   useEffect(() => {
     activityStore.loadActivities();
     // The effect will only re-run if the dependencies have changed since the last render
     // Here, the dependency is activityStore. This means the effect will re-run whenever activityStore changes
   }, [activityStore]);
-
-  function handleDeleteActivity(id: string) {
-    setIsSubmitting(true);
-    // return anything but the id that was passed in
-    agent.Activities.delete(id).then(() => {
-      setActivities([...activities.filter((x) => x.id !== id)]);
-      setIsSubmitting(false);
-    });
-  }
 
   if (activityStore.loadingInitial)
     return <LoadingComponent content="Loading app" />;
@@ -39,11 +24,7 @@ function App() {
     <Fragment>
       <NavBar />
       <Container style={{ marginTop: "7em" }}>
-        <ActivityDashboard
-          activities={activityStore.activities}
-          deleteActivity={handleDeleteActivity}
-          isSubmitting={isSubmitting}
-        />
+        <ActivityDashboard />
       </Container>
     </Fragment>
   );
