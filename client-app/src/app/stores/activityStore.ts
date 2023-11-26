b10c26c894a7ae57d3038a8e01b1ad331ac71ada
oucar <hello@ucaronur.com>
2023-11-26 17:06:53 -0500

    Refactoring the app, adding global state logic using MobX

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,11 +1,54 @@
-import { makeObservable, observable } from "mobx";
+import { makeAutoObservable } from "mobx";
+import { Activity } from "../models/activity";
+import agent from "../api/agent";
 
 export default class ActivityStore {
-  title = "Hello from MobX!";
+  activities: Activity[] = [];
+  selectedActivity: Activity | undefined = undefined;
+  editMode = false;
+  loading = false;
+  loadingInitial = false;
 
   constructor() {
-    makeObservable(this, {
-      title: observable,
-    });
+    makeAutoObservable(this);
   }
+
+  // using an array for actions automatically binds the action to the class
+  loadActivities = async () => {
+    this.setLoadingInitial(true);
+
+    try {
+      const activities = await agent.Activities.list();
+      activities.forEach((activity) => {
+        activity.date = activity.date.split("T")[0];
+        this.activities.push(activity);
+      });
+      this.setLoadingInitial(false);
+    } catch (error) {
+      console.log(`ERROR Loading Activities: ${error}`);
+      this.setLoadingInitial(false);
+    }
+  };
+
+  setLoadingInitial = (state: boolean) => {
+    this.loadingInitial = state;
+  };
+
+  selectActivity = (id: string) => {
+    this.selectedActivity = this.activities.find((x) => x.id === id) ?? undefined;
+  };
+
+  cancelSelectedActivity = () => {
+    this.selectedActivity = undefined;
+  };
+
+  // id is optional since for a new activity we don't have an id yet
+  openForm = (id?: string) => {
+    id ? this.selectActivity(id) : this.cancelSelectedActivity();
+    this.editMode = true;
+  };
+
+  closeForm = () => {
+    this.editMode = false;
+  };
 }
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,62 +1,44 @@
-import React from "react";
-import { Grid, List } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import ActivityList from "./ActivityList";
 import ActivityDetails from "../details/ActivityDetails";
 import ActivityForm from "../form/ActivityForm";
+import { useStore } from "../../../app/stores/store";
+import { observer } from "mobx-react-lite";
 
 // props we got from App.tsx
 interface Props {
   activities: Activity[];
-  selectedActivity: Activity | undefined;
-  editMode: boolean;
-
-  selectActivity: (id: string) => void;
-  cancelSelectActivity: () => void;
-  openForm: (id: string) => void;
-  closeForm: () => void;
   createOrEdit: (activity: Activity) => void;
   deleteActivity: (id: string) => void;
   isSubmitting: boolean;
 }
 
 // destructing activities object from Props
-export default function ActivityDashboard({
+export default observer(function ActivityDashboard({
   activities,
-  selectedActivity,
-  selectActivity,
-  cancelSelectActivity,
-  editMode,
-  openForm,
-  closeForm,
   createOrEdit,
   deleteActivity,
   isSubmitting,
 }: Props) {
+  const { activityStore } = useStore();
+  const { selectedActivity, editMode } = activityStore;
+
   return (
     <Grid>
       {/* Semantic UI has its grid in 16 columns and not 12! */}
       <Grid.Column width="10">
         <ActivityList
           activities={activities}
-          selectActivity={selectActivity}
           deleteActivity={deleteActivity}
           isSubmitting={isSubmitting}
         />
       </Grid.Column>
       <Grid.Column width="6">
         {/* anything to the right of this will be executed as long as it's truthy */}
-        {selectedActivity && !editMode && (
-          <ActivityDetails
-            activity={selectedActivity}
-            cancelSelectActivity={cancelSelectActivity}
-            openForm={openForm}
-          />
-        )}
+        {selectedActivity && !editMode && <ActivityDetails />}
         {editMode && (
           <ActivityForm
-            closeForm={closeForm}
-            activity={selectedActivity}
             createOrEdit={createOrEdit}
             isSubmitting={isSubmitting}
           />
@@ -64,4 +46,4 @@ export default function ActivityDashboard({
       </Grid.Column>
     </Grid>
   );
-}
+});
