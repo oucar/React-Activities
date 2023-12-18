c4c25d38b3a6e53042ce2f388854419329c76568
oucar <onurucar@iu.edu>
2023-12-17 21:56:44 -0500

    Done implementing the initial router, moved home page out of the router.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,17 +2,25 @@ import { Fragment } from "react";
 import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
 import { observer } from "mobx-react-lite";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
 
 function App() {
+  const location = useLocation();
   return (
     <Fragment>
-      <NavBar />
-      <Container style={{ marginTop: "7em" }}>
-        {/* <ActivityDashboard /> */}
-        {/* Similar to Angular */}
-        <Outlet />
-      </Container>
+      {location.pathname === "/" ? (
+        <HomePage />
+      ) : (
+        <>
+          <NavBar />
+          <Container style={{ marginTop: "7em" }}>
+            {/* <ActivityDashboard /> */}
+            {/* Similar to Angular */}
+            <Outlet />
+          </Container>
+        </>
+      )}
     </Fragment>
   );
 }
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,15 +1,31 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { Button, Form, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { Activity } from "../../../app/models/activity";
+import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { v4 as uuid } from "uuid";
 
 // Since Loading is an observable we have in activityStore,
 // The whole component function needs to be an observable.
 export default observer(function ActivityForm() {
   const { activityStore } = useStore();
-  const { selectedActivity, createActivity, updateActivity, loading} = activityStore;
+  const {
+    selectedActivity,
+    createActivity,
+    updateActivity,
+    loading,
+    loadActivity,
+    loadingInitial,
+  } = activityStore;
 
-  const initialState = selectedActivity ?? {
+  // getting the id from the parameters
+  const { id } = useParams<{ id: string }>();
+
+  const navigate = useNavigate();
+
+  const [activity, setActivity] = useState<Activity>({
     id: "",
     title: "",
     category: "",
@@ -18,12 +34,25 @@ export default observer(function ActivityForm() {
     city: "",
     state: "",
     venue: "",
-  };
+  });
 
-  const [activity, setActivity] = useState(initialState);
+  // useEffect hook is used to load an activity when the component mounts or when the id or loadActivity function changes.
+  useEffect(() => {
+    if (id) loadActivity(id).then((activity) => setActivity(activity!));
+  }, [id, loadActivity]);
 
   function handleSubmit() {
-    activity.id ? updateActivity(activity) : createActivity(activity);
+    // Create or update accordingly, then navigate to the activity details page
+    if (!activity.id) {
+      activity.id = uuid();
+      createActivity(activity).then(() =>
+        navigate(`/activities/${activity.id}`)
+      );
+    } else {
+      updateActivity(activity).then(() =>
+        navigate(`/activities/${activity.id}`)
+      );
+    }
   }
 
   function handleInputChange(
@@ -33,6 +62,9 @@ export default observer(function ActivityForm() {
     const { name, value } = event.target;
     setActivity({ ...activity, [name]: value });
   }
+
+  // if the loadingInitial is true, we return a loading component
+  if (loadingInitial) return <LoadingComponent content="Loading activity..." />;
 
   return (
     // the clearing attribute on a Segment component is used to automatically
@@ -90,6 +122,8 @@ export default observer(function ActivityForm() {
           content="Submit"
         />
         <Button
+          as={Link}
+          to="/activities"
           floated="right"
           type="button"
           content="Cancel"
