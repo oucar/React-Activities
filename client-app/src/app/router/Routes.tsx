eaba57c30744362a405e690f7714f130271942d6
oucar <onurucar@iu.edu>
2023-12-31 22:58:30 -0500

    Implemented error handling by introducing an exception controller

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -1,20 +1,27 @@
-import { RouteObject, createBrowserRouter } from "react-router-dom";
+import { Navigate, RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../layout/App";
-import HomePage from "../../features/home/HomePage";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import NotFound from "../../features/errors/NotFound";
+import TestError from "../../features/errors/TestError";
+import ServerError from "../../features/errors/ServerError";
 
 export const routes: RouteObject[] = [
   {
     path: "/",
     element: <App />,
     children: [
-        {path: "activities", element: <ActivityDashboard />},
-        {path: "activities/:id", element: <ActivityDetails />},
-        {path: 'createActivity', element: <ActivityForm key={'create'}/>},
-        {path: 'manage/:id', element: <ActivityForm key={'manage'}/>},
-    ]
+      { path: "activities", element: <ActivityDashboard /> },
+      { path: "activities/:id", element: <ActivityDetails /> },
+      { path: "createActivity", element: <ActivityForm key={"create"} /> },
+      { path: "manage/:id", element: <ActivityForm key={"manage"} /> },
+      { path: "errors", element: <TestError /> },
+      { path: "not-found", element: <NotFound /> },
+      { path: "server-error", element: <ServerError /> },
+      // Everything else will be redirected to the NotFound page
+      { path: "*", element: <Navigate replace to="/not-found" /> },
+    ],
   },
 ];
 
