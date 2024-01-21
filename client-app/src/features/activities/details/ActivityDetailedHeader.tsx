04546eaa2445025b8027af28e7723656e2c16f8b
oucar <hello@ucaronur.com>
2024-01-20 20:30:20 -0500

    Done implementing Formink for reusable input fields

--- client-app/src/features/activities/details/ActivityDetailedHeader.tsx
+++ client-app/src/features/activities/details/ActivityDetailedHeader.tsx
@@ -2,6 +2,7 @@ import { observer } from 'mobx-react-lite';
 import {Button, Header, Item, Segment, Image} from 'semantic-ui-react'
 import {Activity} from "../../../app/models/activity";
 import { Link } from 'react-router-dom';
+import { format } from 'date-fns';
 
 // dimming the activity image
 const activityImageStyle = {
@@ -37,7 +38,7 @@ export default observer (function ActivityDetailedHeader({activity}: Props) {
                                     content={activity.title}
                                     style={{color: 'white'}}
                                 />
-                                <p>{activity.date}</p>
+                                <p>{format(activity.date!, "dd MMM yyyy")}</p>
                                 <p>
                                     Hosted by <strong>Bob</strong>
                                 </p>
