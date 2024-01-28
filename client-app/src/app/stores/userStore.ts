173f4e07d21767971dfad81d89bf2643393faae4
oucar <onurucar@iu.edu>
2024-01-27 23:26:21 -0500

    implemented initial login & logout functionality

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -1,6 +1,8 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
 import { User, UserFormValues } from "../models/user";
 import agent from "../api/agent";
+import { store } from "./store";
+import { router } from "../router/Routes";
 
 export default class UserStore {
   user: User | null = null;
@@ -17,7 +19,17 @@ export default class UserStore {
 
   login = async (creds: UserFormValues) => {
     const user = await agent.Account.login(creds);
-    // store.commonStore.setToken(user.token);
-    console.log(user);
+    store.commonStore.setToken(user.token);
+
+    // Happens after the token is set, in the next tick
+    runInAction(() => (this.user = user));
+    router.navigate("/activities");
+  };
+
+  logout = () => {
+    store.commonStore.setToken(null);
+    window.localStorage.removeItem("jwt");
+    this.user = null;
+    router.navigate("/");
   };
 }
