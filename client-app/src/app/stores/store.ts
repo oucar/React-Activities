9ea933513450dda9d322031f0a1b9ff4f5c6c884
oucar <hello@ucaronur.com>
2024-02-03 23:10:55 -0500

    Adding modals for login and register forms

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -2,11 +2,13 @@ import { createContext, useContext } from "react";
 import ActivityStore from "./activityStore";
 import CommonStore from "./commonStore";
 import UserStore from "./userStore";
+import ModalStore from "./modalStore";
 
 interface Store {
   activityStore: ActivityStore;
   commonStore: CommonStore;
   userStore: UserStore;
+  modalStore: ModalStore;
 
 }
 
@@ -14,6 +16,7 @@ export const store: Store = {
   activityStore: new ActivityStore(),
   commonStore: new CommonStore(),
   userStore: new UserStore(),
+  modalStore: new ModalStore(),
 };
 
 export const StoreContext = createContext(store);
--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -1,6 +1,6 @@
 import { ErrorMessage, Form, Formik } from "formik";
 import CustomTextInput from "../../app/common/form/CustomTextInput";
-import { Button, Label } from "semantic-ui-react";
+import { Button, Header, Label } from "semantic-ui-react";
 import { useStore } from "../../app/stores/store";
 import { observer } from "mobx-react-lite";
 
@@ -16,8 +16,15 @@ export default observer(function LoginForm() {
           .catch((error) => setErrors({ error: "Invalid email or password!" }))
       }
     >
+      {/* TODO: CUSTOMIZE THIS FURTHER IN THE FUTURE */}
       {({ handleSubmit, isSubmitting, errors }) => (
         <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
+          <Header
+            as="h2"
+            content="Login to Reactivities"
+            color="teal"
+            textAlign="center"
+          />
           <CustomTextInput name="email" placeholder="Email" />
           <CustomTextInput
             name="password"
