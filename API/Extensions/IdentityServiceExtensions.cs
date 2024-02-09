babc63df45ccfbf948910da2ffe59ff6c083a83c
oucar <hello@ucaronur.com>
2024-02-09 00:25:04 -0500

    Done implementing attendence logic and auth

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -5,7 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using API.Services;
 using Domain;
+using Infrastructure.Security;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
 using Persistence;
 
@@ -14,22 +16,27 @@ namespace API.Extensions
     // static: no need to create instances of it.
     public static class IdentityServiceExtensions
     {
-        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+        public static IServiceCollection AddIdentityServices(
+            this IServiceCollection services,
+            IConfiguration config
+        )
         {
             // AddIdentityCore: only adds the bare minimum of what we need to get up and running
             // Everything related to authentication and authorization is going to be added to the AddIdentityCore method
-            services.AddIdentityCore<AppUser>(opt =>
-            {
-                // Authentication options
-                opt.Password.RequireNonAlphanumeric = false;
-                opt.User.RequireUniqueEmail = true;
-                
-            }).AddEntityFrameworkStores<DataContext>();
+            services
+                .AddIdentityCore<AppUser>(opt =>
+                {
+                    // Authentication options
+                    opt.Password.RequireNonAlphanumeric = false;
+                    opt.User.RequireUniqueEmail = true;
+                })
+                .AddEntityFrameworkStores<DataContext>();
 
             // TODO: Will be fixed later!!
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
 
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(opt =>
                 {
                     // Add the token validation parameters
@@ -48,10 +55,8 @@ namespace API.Extensions
                     // Add an event to the JwtBearerEvents to handle the token expired exception
                     opt.Events = new JwtBearerEvents
                     {
-
                         OnMessageReceived = context =>
                         {
-
                             // Get the token from the request
                             var accessToken = context.Request.Query["access_token"];
 
@@ -59,28 +64,38 @@ namespace API.Extensions
                             var path = context.HttpContext.Request.Path;
 
                             // Check if the token is not null and if the path starts with /chat
-                            if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/chat"))
+                            if (
+                                !string.IsNullOrEmpty(accessToken)
+                                && path.StartsWithSegments("/chat")
+                            )
                             {
-
                                 // Set the token
                                 context.Token = accessToken;
-
                             }
 
                             return Task.CompletedTask;
-
                         }
-
                     };
-
                 });
 
+            services.AddAuthorization(opt =>
+            {
+                opt.AddPolicy(
+                    "IsActivityHost",
+                    policy =>
+                    {
+                        policy.Requirements.Add(new IsHostRequirement());
+                    }
+                );
+            });
+
+            // Transient makes it run as long as the method is running
+            services.AddTransient<IAuthorizationHandler, IsHostRequirementHandler>();
 
             // AddScoped is for individual HTTP requests - which is exactly what you want with a token
             services.AddScoped<TokenService>();
 
             return services;
-
         }
     }
 }
--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -14,10 +14,12 @@ namespace Application.Activities
         public DateTime Date { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
-        public string State { get; set; }
         public string City { get; set; }
         public string Venue { get; set; }
+        public string State { get; set; }
         public string HostUsername { get; set; }
+        public bool IsCancelled { get; set; }
+
         public ICollection<Profile> Attendees { get; set; }
     }
 }
