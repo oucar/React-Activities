644df8d71b219672a375f4bf4ef624a091564c5a
Onur Ucar <hello@ucaronur.com>
2024-02-14 12:14:16 -0500

    Authenticating to SignalR

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -51,6 +51,23 @@ namespace API.Extensions
                         // ValidateAudience: validate the audience of the token
                         ValidateAudience = false,
                     };
+                    
+                    opt.Events = new JwtBearerEvents
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            var accessToken = context.Request.Query["access_token"];
+                            var path = context.HttpContext.Request.Path;
+                            if (
+                                !string.IsNullOrEmpty(accessToken)
+                                && (path.StartsWithSegments("/chat"))
+                            )
+                            {
+                                context.Token = accessToken;
+                            }
+                            return Task.CompletedTask;
+                        }
+                    };
 
                     // Add an event to the JwtBearerEvents to handle the token expired exception
                     opt.Events = new JwtBearerEvents
