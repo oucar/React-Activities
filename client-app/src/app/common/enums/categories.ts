fe0f208bae335a8451690d773058e2ff626f0be3
Onur Ucar <hello@ucaronur.com>
2024-02-19 22:51:00 -0500

    Added icons for the categories, added dynamic rendering

--- client-app/src/app/common/enums/categories.ts
+++ client-app/src/app/common/enums/categories.ts
@@ -1,8 +1,17 @@
 export enum Category {
-    Drinks = "drinks",
-    Culture = "culture",
-    Film = "film",
-    Food = "food",
-    Music = "music",
-    Travel = "travel",
-  }
+  Drinks = "drinks",
+  Culture = "culture",
+  Film = "film",
+  Food = "food",
+  Music = "music",
+  Travel = "travel",
+}
+
+export enum CategoryIcons {
+  Drinks = "beer",
+  Culture = "book",
+  Film = "film",
+  Food = "utensils",
+  Music = "music",
+  Travel = "globe",
+}
